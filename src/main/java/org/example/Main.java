9d1d43a56e1911261f9a406a1f157579e5cb79a7
Henry Nguyen <nguyenhenry682@gmail.com>
2024-02-04 14:27:28 -0600

    added retrieval functionality

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,20 @@
 package org.example;
+import org.example.data.Product;
 import org.jsoup.*;
 import org.jsoup.nodes.*;
 import org.jsoup.select.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Document doc;
 
         try{
-            // Connects to URL and gets a Jsoup HTML document object.
-            // Bypass website blockers with userAgent and header.
+            // Connects to URL and gets a Jsoup HTML document object
+            // Bypass website blockers with userAgent and header
             doc = Jsoup
                     .connect("https://scrapeme.live/shop")
                     .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
@@ -21,5 +24,28 @@ public class Main {
         catch(IOException e){
             throw new RuntimeException(e);
         }
+
+        // Retrieve list of product HTML elements
+        Elements products = doc.select("li.product");
+
+        // Initialize list to store Java object of scraped data
+        List<Product> productList = new ArrayList<>();
+
+        // Iterate over list of HTML products
+        for(Element product: products){
+            Product productObject = new Product();
+
+            // Extract data of interest from HTMl element and store it in new object
+            productObject.setUrl(product.selectFirst("a").attr("href"));
+            productObject.setImage(product.selectFirst("img").attr("src"));
+            productObject.setName(product.selectFirst("h2").text());
+            productObject.setPrice(product.selectFirst("span").text());
+
+            // Add new object to list of scraped products
+            productList.add(productObject);
+        }
+
+        productList.toString();
+        System.out.println(productList);
     }
 }
---
+++ src/main/java/org/example/data/Product.java
@@ -0,0 +1,49 @@
+package org.example.data;
+
+public class Product {
+    private String url;
+    private String image;
+    private String name;
+    private String price;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    // Convert string to JSON
+    @Override
+    public String toString(){
+        return "{ \"url\":\"" + url + "\", "
+                + " \"image\": \"" + image + "\", "
+                + "\"name\":\"" + name + "\", "
+                + "\"price\": \"" + price + "\" } \n";
+    }
+}
